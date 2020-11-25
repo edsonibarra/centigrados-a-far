ca42469dd1c8a23d4ffad33cf481083684cec5df
Oscar Islas <oislasreyes@gmail.com>
2020-11-24 20:06:17 -0600

    Estructura de la actividad con hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Slider from "@material-ui/core/Slider";
 
-function App() {
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function App() {
+  //1. Agregar los estados para convertir los grados centigrados en grados farenheit
+
+  const cambiarGrados = (evento, valor) => {
+    //2. Completar la logica para actualizar el estado de centigrados y farenheit y convertir el valor en grados centigrados a grados farenheit
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="temperature">
+        <h1>Convertir °C a °F</h1>
+        <h5>{0}°F</h5>
+        {
+          //3. Definir los props faltantes para el componente Slider
+        }
+        <Slider
+          defaultValue={0}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={10}
+          marks
+          min={10}
+          max={110}
+          onChange="funcion"
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
