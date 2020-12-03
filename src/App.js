93d4a70b915e5e6e6a022035bd15a36e901566b7
edson <edsonibarra97@gmail.com>
2020-12-03 01:20:52 -0600

    click arreglado

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Slider from "@material-ui/core/Slider";
 
 function valuetext(value) {
   return `${value}°C`;
 }
+//Formula para convertir centigrados a farenheit. 
+// ( C * 9/5 ) + 32 = F
 
 export default function App() {
   //1. Agregar los estados para convertir los grados centigrados en grados farenheit
+  const[c, setC] = useState(10)
+  let gradosFar = (c * 9/5) + 32
+  const[f, setF] = useState(gradosFar)
 
   const cambiarGrados = (evento, valor) => {
     //2. Completar la logica para actualizar el estado de centigrados y farenheit y convertir el valor en grados centigrados a grados farenheit
+    setC(valor)
+    console.log(c)
+    
+    setF((valor * 9/5) + 32)
+    console.log(`grados far${f}`)
+    
   };
 
   return (
     <div className="App">
       <div className="temperature">
         <h1>Convertir °C a °F</h1>
-        <h5>{0}°F</h5>
+        <h5>{f}°F</h5>
         {
           //3. Definir los props faltantes para el componente Slider
         }
         <Slider
           defaultValue={0}
-          getAriaValueText={valuetext}
+          getAriaValueText={()=>valuetext(f)}
           aria-labelledby="discrete-slider"
           valueLabelDisplay="auto"
           step={10}
           marks
           min={10}
           max={110}
-          onChange="funcion"
+          onChange={cambiarGrados}
         />
       </div>
     </div>
